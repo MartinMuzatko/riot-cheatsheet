f5c7d8680592f7c430d6db48e8a7d2b836cc5b85
Martin Muzatko <martin@happy-css.com>
2016-06-28 16:03:37 +0200

    Added markdown tag to require md files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import html from './README.md'
 import './less/main.less'
 import './js/tags/app.html'
 import './js/tags/cheatsheet.html'
+import './js/tags/markdown.html'
 
 document.body.innerHTML = `<app>${html}</app>`
 riot.mount('*')
