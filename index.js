12d091e35e7dc6535e2e304b313a2fdc63cd13d1
Martin Muzatko <martin@happy-css.com>
2016-06-30 11:47:54 +0200

    Added highlight tag to highlight the searched string

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Waypoint from './node_modules/waypoints/lib/noframework.waypoints.js'
 import './less/main.less'
 import './js/tags/app.html'
 import './js/tags/markdown.html'
+import './js/tags/highlight.html'
 
 document.body.innerHTML = '<app></app>'
 riot.mount('*')
